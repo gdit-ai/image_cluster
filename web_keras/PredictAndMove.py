db50b115e443e838a2cc77d70cb1645a3b35279e
gdit-ai <58966757+gdit-ai@users.noreply.github.com>
2019-12-17 15:04:29 +0800

    Update PredictAndMove.py

--- web_keras/PredictAndMove.py
+++ web_keras/PredictAndMove.py
@@ -4,28 +4,21 @@ from keras.models import load_model
 import numpy as np
 import sys
 
-average = 82.1400742479
-
 
 def LoadImage(image_path):
     img = cv2.imread(image_path)
     img = cv2.resize(img, dsize=(32, 32), interpolation=cv2.INTER_AREA)
-    img = img.astype("float32")
-    img -= average
-    img /= 255.
+    img = img.astype("float32") / 255.0
+
     return np.array(img[:, :, :3])
 
 
-def abs_path(path):
-    model = load_model(r"./vggmodel_65-0.00-1.00.hdf5")
-    label_dict = {0: "air", 1: "car", 2: "bird", 3: "cat", 4: "deer", 5: "dog", 6: "drop", 7: "horse", 8: "ship",
-                  9: "truck"}
+def predict(path):
+    model = load_model(r"vggmodel_21-0.00-0.98.hdf5")
+    label_dict={0:'Animal', 1:'Architecture', 2:'Scenery', 3:'people', 4:'plane'}
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer=keras.optimizers.Adadelta()
                   , metrics=["accuracy"])
-    class_pic = []
-    # dir_list = [r'.\static\upload\taidi_2.jpg', r'.\static\upload\taidi_2.jpg']
-    # for i in range(len(dir_list)):
     img = LoadImage(path)
     res = np.argmax(model.predict(np.array([img])))
     pic_class = label_dict[int(res)]
@@ -34,4 +27,4 @@ def abs_path(path):
 
 if __name__ == "__main__":
     path = sys.argv[1]
-    abs_path(path)
+    predict(path)
